087664199a755a55c20e100160423d662974f08d
Roman Mazuryk <romazuryk@gmail.com>
2026-02-25 23:53:43 +0100

    Update eslint.config.js with new configuration

--- frontend-react/eslint.config.js
+++ frontend-react/eslint.config.js
@@ -15,7 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.jest }, // add jest globals
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
